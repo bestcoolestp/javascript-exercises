f67c96021391ea3e528106683ee9d2582281eea2
bestcoolestp <108534975+bestcoolestp@users.noreply.github.com>
2023-03-13 09:32:44 +0900

    exercise1 & 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    repeatedStr ="";
+    for(let i = 0; i < num; i++) {
+        repeatedStr += string
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
