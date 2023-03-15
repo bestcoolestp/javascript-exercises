f1f966cfb2e6aca395c290c17b2f13d6551b5f4d
bestcoolestp <108534975+bestcoolestp@users.noreply.github.com>
2023-03-15 10:07:33 +0900

    exercise4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(item => args.indexOf(item) === -1);
 };
 
 // Do not edit below this line
