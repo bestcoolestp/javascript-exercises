7defbdb46c3ef7550b8068c24dad9356aa5b89cb
bestcoolestp <108534975+bestcoolestp@users.noreply.github.com>
2023-03-17 13:32:58 +0900

    exercise6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    if(givenYear % 4 === 0 && (givenYear % 100 !== 0 || givenYear % 400 === 0)) {
+        return `${givenYear} is a leap year`;
+    } else {
+        return `${givenYear} is Not a year `;
+    }
 };
 
 // Do not edit below this line
